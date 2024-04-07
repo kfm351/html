187e160ab29b8427ddb47e03ba01dc3341c4933a
Kfm351 <kfm351@yandex.ru>
2024-04-08 02:00:47 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp1/Data/db_connection.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorApp1.Data
+{
+    public class db_connection
+    {
+        public List<Student> students = new List<Student>()
+        {
+            new Student("Ivanov Ivan", 530),
+            new Student("Petrov Petr",510),
+            new Student("Sidorov Sergay",660)
+        };
+    }
+
+    public class Student
+    {
+        public Student(string name, int group)
+        {
+            Id = Guid.NewGuid().ToString();
+
+            Name = name;
+            Group = group;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Group { get; set; }
+    }
+}
